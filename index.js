cb3d901c32eb3517441bda29f0d6f7da2b94b265
Nitin Sharma <nitinsharma@BigAdmins-Mac-mini.local>
2018-03-13 18:09:42 +0530

    first ping pong

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+
+
+const app = express()
+
+console.log(app);
+
+
+
+app.get('/', (req, res) => {
+	console.log('working');
+	res.send('Hello World!')
+})
+
+app.use((req, res) => {
+	console.log(req);
+})
+
+var server = app.listen(8081, function () {
+
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
