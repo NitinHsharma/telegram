6b85d85786b774b3282ba476c57aba447b7269a9
Nitin Sharma <nitinsharma@BigAdmins-Mac-mini.local>
2018-03-14 17:16:18 +0530

    heroku take port number dynamically

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res) => {
 	console.log(req);
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
 
    var host = server.address().address
    var port = server.address().port
