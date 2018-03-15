576051c02f464467aef3f517fda608f3dc1fc0a9
Nitin Sharma <nitinsharma@BigAdmins-Mac-mini.local>
2018-03-15 19:49:18 +0530

    changes for wiki API

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const http = require('http')
 
 
 const app = express()
 
-console.log(app);
+
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-	
+
 	res.send('pong')
 })
 
@@ -18,13 +19,50 @@ app.get('/start', (req, res) => {
 	res.send('Hello')
 })
 
-app.post('/hook',(req,res)=>{
-	console.log('Come here',req.body);
-	res.send({
-					method: 'sendMessage',
-					chat_id: 480882720,
-					text: 'You said ' + req.body.message.text
-				});
+app.post('/hook', (req, res) => {
+
+	let message = req.body.message;
+	let chat_id = message.chat.id;
+	let user_name = (message.from.first_name !== '' ? message.from.first_name : 'Guest');
+
+	if (message.text) {
+		let text = message.text;
+
+		if (text.indexOf(wiki) >= 0) {
+			let userText = text.split(' ');
+
+			http.get(`https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=${userText[1]}&format=json`, (err, data)=> {
+				if (err) {
+					res.send({
+						method: 'sendMessage',
+						chat_id: chat_id,
+						text: `You said ${text}`
+					});
+				} else {
+					res.send({
+						method: 'sendMessage',
+						chat_id: chat_id,
+						text: `${data}`
+					});
+				}
+			});
+		} //https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=pizza&format=json
+
+
+		res.send({
+			method: 'sendMessage',
+			chat_id: chat_id,
+			text: `You said ${text}`
+		});
+	}
+
+
+
+res.send({
+	method: 'sendMessage',
+	chat_id: chat_id,
+	text: 'You said ' + req.body.message.text
+});
 })
 
 
@@ -33,11 +71,10 @@ app.use((req, res) => {
 	console.log(req);
 })
 
-var server = app.listen(process.env.PORT || 5000, function () {
+var server = app.listen(process.env.PORT || 5000, function() {
 
-   var host = server.address().address
-   var port = server.address().port
+	var host = server.address().address
+	var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
+	console.log("Example app listening at http://%s:%s", host, port)
 })
-
